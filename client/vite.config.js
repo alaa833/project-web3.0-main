7c294807ad13a327ba3ca877817863609bf81a5c
alaa833 <loloshady1999@gmail.com>
2023-07-12 19:58:21 +0300

    first comit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/project-web3.0",
+  base: "/project-web3.0-main",
   plugins: [react()]
 })
